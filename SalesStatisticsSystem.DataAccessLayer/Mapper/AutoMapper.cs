57643ff6d975e3887871c9981d1f36536e1b4d6e
AlexeyShpavda <alexeyshpavda@gmail.com>
2019-01-02 19:16:19 +0300

    Create Method to Generate Mapper Configuration

---
+++ SalesStatisticsSystem.DataAccessLayer/Mapper/AutoMapper.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using SalesStatisticsSystem.Contracts.Core.DataTransferObjects;
+using SalesStatisticsSystem.Entity;
+
+namespace SalesStatisticsSystem.DataAccessLayer.Mapper
+{
+    internal static class AutoMapper
+    {
+        internal static MapperConfiguration CreateConfiguration()
+        {
+            return new MapperConfiguration(config =>
+            {
+                config.CreateMap<Customer, CustomerDto>();
+                config.CreateMap<CustomerDto, Customer>();
+
+                config.CreateMap<Product, ProductDto>();
+                config.CreateMap<ProductDto, Product>();
+
+                config.CreateMap<Manager, ManagerDto>();
+                config.CreateMap<ManagerDto, Manager>();
+
+                config.CreateMap<Sale, SaleDto>();
+                config.CreateMap<SaleDto, Sale>()
+                    .ForMember(x => x.Customer, opt => opt.Ignore())
+                    .ForMember(x => x.Manager, opt => opt.Ignore())
+                    .ForMember(x => x.Product, opt => opt.Ignore());
+            });
+        }
+    }
+}
