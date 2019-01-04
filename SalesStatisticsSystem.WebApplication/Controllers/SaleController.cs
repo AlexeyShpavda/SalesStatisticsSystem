111a6b3e0554c74298e640563af56feb6cc3ccd8
AlexeyShpavda <alexeyshpavda@gmail.com>
2019-01-04 22:39:50 +0300

    Add Sale Controller to Web Application

---
+++ SalesStatisticsSystem.WebApplication/Controllers/SaleController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace SalesStatisticsSystem.WebApplication.Controllers
+{
+    public class SaleController : Controller
+    {
+        // GET: Sale
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
