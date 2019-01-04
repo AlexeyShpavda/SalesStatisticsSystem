c1c2398611147ba8217b42a7ee881fea6dbe1389
AlexeyShpavda <alexeyshpavda@gmail.com>
2019-01-04 22:16:06 +0300

    Create SaleService Class in Business Logic Layer

---
+++ SalesStatisticsSystem.Core/Services/SaleService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using SalesStatisticsSystem.Contracts.Core.DataTransferObjects;
+using SalesStatisticsSystem.Contracts.DataAccessLayer.UnitOfWorks;
+using SalesStatisticsSystem.DataAccessLayer.UnitOfWorks;
+using SalesStatisticsSystem.Entity;
+
+namespace SalesStatisticsSystem.Core.Services
+{
+    public class SaleService : IDisposable
+    {
+        private SalesInformationEntities Context { get; }
+
+        private ReaderWriterLockSlim Locker { get; }
+
+        private ISaleUnitOfWork SaleUnitOfWork { get; }
+
+        public SaleService()
+        {
+            Context = new SalesInformationEntities();
+
+            Locker = new ReaderWriterLockSlim();
+
+            SaleUnitOfWork = new SaleUnitOfWork(Context, Locker);
+        }
+
+
+        public IEnumerable<SaleDto> GetSales()
+        {
+            return SaleUnitOfWork.GetAll();
+        }
+
+        private bool _disposed;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    Locker.Dispose();
+                    Context.Dispose();
+                }
+            }
+            _disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
