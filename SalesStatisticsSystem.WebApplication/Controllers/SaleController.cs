d69205c242aa79e03cbe038b099a80f1089ea8d4
AlexeyShpavda <alexeyshpavda@gmail.com>
2019-01-05 19:43:38 +0300

    Create Mapper to Transform DTO Models into View Models

--- SalesStatisticsSystem.WebApplication/Controllers/SaleController.cs
+++ SalesStatisticsSystem.WebApplication/Controllers/SaleController.cs
@@ -1,6 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Web.Mvc;
+using AutoMapper;
 using SalesStatisticsSystem.Core.Services;
+using SalesStatisticsSystem.WebApplication.Models;
 
 namespace SalesStatisticsSystem.WebApplication.Controllers
 {
@@ -8,14 +11,20 @@ namespace SalesStatisticsSystem.WebApplication.Controllers
     {
         private readonly SaleService _saleService;
 
+        private readonly IMapper _mapper;
+
         public SaleController()
         {
+            _mapper = Support.AutoMapper.CreateConfiguration().CreateMapper();
+
             _saleService = new SaleService();
         }
 
         public async Task<ActionResult> Index()
         {
             var sales = await _saleService.GetSalesAsync();
+
+            var salesViewModels = _mapper.Map<IEnumerable<SaleViewModel>>(sales);
 
             return View(sales);
         }
