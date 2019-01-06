fc65b83b6f810e6edfee02bc7e3aad8d1e5faaf8
AlexeyShpavda <alexeyshpavda@gmail.com>
2019-01-06 20:03:37 +0300

    Create Customer Controller for Web Application

---
+++ SalesStatisticsSystem.WebApplication/Controllers/CustomerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Web.Mvc;
+
+namespace SalesStatisticsSystem.WebApplication.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                // TODO: make delete by id
+
+                Delete(id);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
