65e42dae62e18fe00dc9b68725b9603edf9518c0
AlexeyShpavda <alexeyshpavda@gmail.com>
2019-01-07 01:38:00 +0300

    Create View for Create Method of Sale Controller

--- SalesStatisticsSystem.WebApplication/Controllers/SaleController.cs
+++ SalesStatisticsSystem.WebApplication/Controllers/SaleController.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using AutoMapper;
+using SalesStatisticsSystem.Contracts.Core.DataTransferObjects;
 using SalesStatisticsSystem.Contracts.Core.Services;
 using SalesStatisticsSystem.Core.Services;
 using SalesStatisticsSystem.WebApplication.Models;
@@ -28,6 +30,26 @@ namespace SalesStatisticsSystem.WebApplication.Controllers
             var salesViewModels = _mapper.Map<IEnumerable<SaleViewModel>>(salesDto);
 
             return View(salesViewModels);
+        }
+
+        public ActionResult Create()
+        {
+            return View(new SaleViewModel {Date = DateTime.Now});
+        }
+
+        [HttpPost]
+        public ActionResult Create(SaleViewModel sale)
+        {
+            try
+            {
+                _saleService.Add(_mapper.Map<SaleDto>(sale));
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
 
         public ActionResult Delete(int id)
