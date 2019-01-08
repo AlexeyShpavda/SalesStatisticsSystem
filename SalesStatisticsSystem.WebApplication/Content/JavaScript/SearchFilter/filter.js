fd84c3782524a0baee838e7ced64450b968ba9fd
AlexeyShpavda <alexeyshpavda@gmail.com>
2019-01-08 07:05:17 +0300

    Create JavaScript Search Filter

---
+++ SalesStatisticsSystem.WebApplication/Content/JavaScript/SearchFilter/filter.js
@@ -0,0 +1,18 @@
+﻿function searchFilter() {
+    var input = document.getElementById("inputField");
+    var filter = input.value.toUpperCase();
+    var table = document.getElementById("table");
+    var tr = table.getElementsByTagName("tr");
+
+    for (var i = 0; i < tr.length; i++) {
+        var td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            var txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
