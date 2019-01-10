45a114e67626b0201a59f028cb61aaee0ab45ea5
AlexeyShpavda <alexeyshpavda@gmail.com>
2019-01-11 02:00:07 +0300

    Create Class for Sales Filter

---
+++ SalesStatisticsSystem.WebApplication/Models/Filters/SaleFilter.cs
@@ -0,0 +1,18 @@
+﻿namespace SalesStatisticsSystem.WebApplication.Models.Filters
+{
+    public class SaleFilter
+    {
+        public string DateFrom { get; set; } = null;
+        public string DateTo { get; set; } = null;
+
+        public string CustomerFirstName { get; set; } = null;
+        public string CustomerLastName { get; set; } = null;
+
+        public string ProductName { get; set; } = null;
+
+        public string SumFrom { get; set; } = null;
+        public string SumTo { get; set; } = null;
+
+        public string ManagerLastName { get; set; } = null;
+    }
+}
