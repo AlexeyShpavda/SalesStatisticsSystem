df676932beb61c7dfdc3f5a51aa63cd3cb88a8af
AlexeyShpavda <alexeyshpavda@gmail.com>
2019-01-13 21:25:03 +0300

    Implement Pagination for Sale in Controller

--- SalesStatisticsSystem.WebApp/Controllers/SaleController.cs
+++ SalesStatisticsSystem.WebApp/Controllers/SaleController.cs
@@ -8,6 +8,7 @@ using SalesStatisticsSystem.Contracts.Core.DataTransferObjects;
 using SalesStatisticsSystem.Contracts.Core.Services;
 using SalesStatisticsSystem.WebApp.Models.Filters;
 using SalesStatisticsSystem.WebApp.Models.SaleViewModels;
+using X.PagedList;
 
 namespace SalesStatisticsSystem.WebApp.Controllers
 {
@@ -24,25 +25,18 @@ namespace SalesStatisticsSystem.WebApp.Controllers
             _mapper = mapper;
         }
 
-        public async Task<ActionResult> Index()
+        public async Task<ActionResult> Index(int? page)
         {
             try
             {
-                var salesDto = await _saleService.GetAllAsync().ConfigureAwait(false);
-
-                var salesViewModels = _mapper.Map<IEnumerable<SaleViewModel>>(salesDto).ToList();
-
-                #region Chart
-                var uniqueProducts = salesViewModels.Select(x => x.Product.Name).Distinct();
-
-                var productSalesQuantity = uniqueProducts
-                    .Select(product => salesViewModels.Count(x => x.Product.Name == product)).ToList();
-
-                ViewBag.UniqueProducts = uniqueProducts;
-                ViewBag.ProductSalesQuantity = productSalesQuantity;
-                #endregion
-
                 ViewBag.SaleFilter = new SaleFilterModel();
+
+                const int pageSize = 3;
+
+                var salesDto = await _saleService.GetUsingPagedListAsync(page ?? 1, pageSize);
+
+                var salesViewModels =
+                        _mapper.Map<IPagedList<SaleViewModel>>(salesDto);
 
                 return View(salesViewModels);
             }
@@ -58,16 +52,19 @@ namespace SalesStatisticsSystem.WebApp.Controllers
         {
             try
             {
+                const int pageSize = 3;
+
                 if (!ModelState.IsValid)
                 {
-                    var dto = await _saleService.GetAllAsync().ConfigureAwait(false);
+                    var dto = await _saleService.GetUsingPagedListAsync(saleFilterModel.Page ?? 1, pageSize)
+                        .ConfigureAwait(false);
 
-                    var viewModels = _mapper.Map<IEnumerable<SaleViewModel>>(dto);
+                    var viewModels = _mapper.Map<IPagedList<SaleViewModel>>(dto);
 
                     return PartialView("Partial/_SaleTable", viewModels);
                 }
 
-                IEnumerable<SaleDto> salesDto;
+                IPagedList<SaleDto> salesDto;
                 if (saleFilterModel.CustomerFilter.FirstName == null &&
                     saleFilterModel.CustomerFilter.LastName == null &&
                     saleFilterModel.DateFrom == null &&
@@ -77,20 +74,31 @@ namespace SalesStatisticsSystem.WebApp.Controllers
                     saleFilterModel.SumFrom == null &&
                     saleFilterModel.SumTo == null)
                 {
-                    salesDto = await _saleService.GetAllAsync().ConfigureAwait(false);
+                    salesDto = await _saleService.GetUsingPagedListAsync(saleFilterModel.Page ?? 1, pageSize)
+                        .ConfigureAwait(false);
                 }
                 else
                 {
-                    salesDto = await _saleService.FindAsync(x =>
-                        (x.Date >= saleFilterModel.DateFrom && x.Date <= saleFilterModel.DateTo) ||
-                        (x.Sum >= saleFilterModel.SumFrom && x.Sum <= saleFilterModel.SumTo) ||
-                        x.Customer.FirstName.Contains(saleFilterModel.CustomerFilter.FirstName) ||
-                        x.Customer.LastName.Contains(saleFilterModel.CustomerFilter.LastName) ||
-                        x.Manager.LastName.Contains(saleFilterModel.ManagerFilter.LastName) ||
-                        x.Product.Name.Contains(saleFilterModel.ProductFilter.Name)).ConfigureAwait(false);
+                    salesDto = await _saleService.GetUsingPagedListAsync(
+                        saleFilterModel.Page ?? 1, pageSize, x =>
+                            (x.Date >= saleFilterModel.DateFrom && x.Date <= saleFilterModel.DateTo) ||
+                            (x.Sum >= saleFilterModel.SumFrom && x.Sum <= saleFilterModel.SumTo) ||
+                            x.Customer.FirstName.Contains(saleFilterModel.CustomerFilter.FirstName) ||
+                            x.Customer.LastName.Contains(saleFilterModel.CustomerFilter.LastName) ||
+                            x.Manager.LastName.Contains(saleFilterModel.ManagerFilter.LastName) ||
+                            x.Product.Name.Contains(saleFilterModel.ProductFilter.Name)).ConfigureAwait(false);
                 }
 
-                var salesViewModels = _mapper.Map<IEnumerable<SaleViewModel>>(salesDto);
+                var salesViewModels = _mapper.Map<IPagedList<SaleViewModel>>(salesDto);
+
+                ViewBag.SaleFilterCustomerFirstNameValue = saleFilterModel.CustomerFilter.FirstName;
+                ViewBag.SaleFilterCustomerLastNameValue = saleFilterModel.CustomerFilter.LastName;
+                ViewBag.SaleFilterDateFromValue = saleFilterModel.DateFrom;
+                ViewBag.SaleFilterDateToValue = saleFilterModel.DateTo;
+                ViewBag.SaleFilterManagerLastNameValue = saleFilterModel.ManagerFilter.LastName;
+                ViewBag.SaleFilterProductNameValue = saleFilterModel.ProductFilter.Name;
+                ViewBag.SaleFilterSumFromValue = saleFilterModel.SumFrom;
+                ViewBag.SaleFilterSumToValue = saleFilterModel.SumTo;
 
                 return PartialView("Partial/_SaleTable", salesViewModels);
             }
@@ -100,8 +108,86 @@ namespace SalesStatisticsSystem.WebApp.Controllers
 
                 return PartialView("Partial/_SaleTable");
             }
-
         }
+
+        //public async Task<ActionResult> Index()
+        //{
+        //    try
+        //    {
+        //        var salesDto = await _saleService.GetAllAsync().ConfigureAwait(false);
+
+        //        var salesViewModels = _mapper.Map<IEnumerable<SaleViewModel>>(salesDto).ToList();
+
+        //        #region Chart
+        //        var uniqueProducts = salesViewModels.Select(x => x.Product.Name).Distinct();
+
+        //        var productSalesQuantity = uniqueProducts
+        //            .Select(product => salesViewModels.Count(x => x.Product.Name == product)).ToList();
+
+        //        ViewBag.UniqueProducts = uniqueProducts;
+        //        ViewBag.ProductSalesQuantity = productSalesQuantity;
+        //        #endregion
+
+        //        ViewBag.SaleFilter = new SaleFilterModel();
+
+        //        return View(salesViewModels);
+        //    }
+        //    catch (Exception exception)
+        //    {
+        //        ViewBag.Error = exception.Message;
+
+        //        return View();
+        //    }
+        //}
+
+        //public async Task<ActionResult> Find(SaleFilterModel saleFilterModel)
+        //{
+        //    try
+        //    {
+        //        if (!ModelState.IsValid)
+        //        {
+        //            var dto = await _saleService.GetAllAsync().ConfigureAwait(false);
+
+        //            var viewModels = _mapper.Map<IEnumerable<SaleViewModel>>(dto);
+
+        //            return PartialView("Partial/_SaleTable", viewModels);
+        //        }
+
+        //        IEnumerable<SaleDto> salesDto;
+        //        if (saleFilterModel.CustomerFilter.FirstName == null &&
+        //            saleFilterModel.CustomerFilter.LastName == null &&
+        //            saleFilterModel.DateFrom == null &&
+        //            saleFilterModel.DateTo == null &&
+        //            saleFilterModel.ManagerFilter.LastName == null &&
+        //            saleFilterModel.ProductFilter.Name == null &&
+        //            saleFilterModel.SumFrom == null &&
+        //            saleFilterModel.SumTo == null)
+        //        {
+        //            salesDto = await _saleService.GetAllAsync().ConfigureAwait(false);
+        //        }
+        //        else
+        //        {
+        //            salesDto = await _saleService.FindAsync(x =>
+        //                (x.Date >= saleFilterModel.DateFrom && x.Date <= saleFilterModel.DateTo) ||
+        //                (x.Sum >= saleFilterModel.SumFrom && x.Sum <= saleFilterModel.SumTo) ||
+        //                x.Customer.FirstName.Contains(saleFilterModel.CustomerFilter.FirstName) ||
+        //                x.Customer.LastName.Contains(saleFilterModel.CustomerFilter.LastName) ||
+        //                x.Manager.LastName.Contains(saleFilterModel.ManagerFilter.LastName) ||
+        //                x.Product.Name.Contains(saleFilterModel.ProductFilter.Name)).ConfigureAwait(false);
+        //        }
+
+        //        var salesViewModels = _mapper.Map<IEnumerable<SaleViewModel>>(salesDto);
+
+        //        return PartialView("Partial/_SaleTable", salesViewModels);
+        //    }
+        //    catch (Exception exception)
+        //    {
+        //        ViewBag.Error = exception.Message;
+
+        //        return PartialView("Partial/_SaleTable");
+        //    }
+
+        //}
 
         public ActionResult Create()
         {
