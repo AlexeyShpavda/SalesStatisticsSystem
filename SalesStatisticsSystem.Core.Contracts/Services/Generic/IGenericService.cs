6a508ea41c9bf08c3f353831f84f596e8a54915c
AlexeyShpavda <alexeyshpavda@gmail.com>
2019-01-19 18:39:31 +0300

    Create Service Interfaces in Core Contracts Assembly

---
+++ SalesStatisticsSystem.Core.Contracts/Services/Generic/IGenericService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using System.Web.Helpers;
+using SalesStatisticsSystem.Core.Contracts.Models.Abstract;
+using X.PagedList;
+
+namespace SalesStatisticsSystem.Core.Contracts.Services.Generic
+{
+    public interface IGenericService<TCoreModel> where TCoreModel : CoreModel
+    {
+        Task<IPagedList<TCoreModel>> GetUsingPagedListAsync(int pageNumber, int pageSize,
+            Expression<Func<TCoreModel, bool>> predicate = null, SortDirection sortDirection = SortDirection.Ascending);
+
+        Task<TCoreModel> GetAsync(int id);
+
+        Task<TCoreModel> AddAsync(TCoreModel model);
+
+        Task<TCoreModel> UpdateAsync(TCoreModel model);
+
+        Task DeleteAsync(int id);
+
+        Task<IEnumerable<TCoreModel>> FindAsync(Expression<Func<TCoreModel, bool>> predicate);
+    }
+}
---
+++ SalesStatisticsSystem.Core.Contracts/Services/ISaleService.cs
@@ -0,0 +1,9 @@
+﻿using SalesStatisticsSystem.Core.Contracts.Models;
+using SalesStatisticsSystem.Core.Contracts.Services.Generic;
+
+namespace SalesStatisticsSystem.Core.Contracts.Services
+{
+    public interface ISaleService : IGenericService<SaleCoreModel>
+    {
+    }
+}
