38a9f2c0e8172382bc2a4aff8bbdfc8a97dedae9
AlexeyShpavda <alexeyshpavda@gmail.com>
2019-01-19 18:50:13 +0300

    Add ReaderWriter Interfaces into DAL Contracts

---
+++ SalesStatisticsSystem.DataAccessLayer.Contracts/ReaderWriter/Generic/IGenericDbReaderWriter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using System.Web.Helpers;
+using SalesStatisticsSystem.Core.Contracts.Models.Abstract;
+using X.PagedList;
+
+namespace SalesStatisticsSystem.DataAccessLayer.Contracts.ReaderWriter.Generic
+{
+    public interface IGenericDbReaderWriter<TCoreModel> where TCoreModel : CoreModel
+    {
+        Task<TCoreModel> AddAsync(TCoreModel model);
+
+        Task<TCoreModel> UpdateAsync(TCoreModel model);
+
+        Task DeleteAsync(int id);
+
+        Task<IPagedList<TCoreModel>> GetUsingPagedListAsync(int pageNumber, int pageSize,
+            Expression<Func<TCoreModel, bool>> predicate = null, SortDirection sortDirection = SortDirection.Ascending);
+
+        Task<TCoreModel> GetAsync(int id);
+
+        Task<IEnumerable<TCoreModel>> FindAsync(Expression<Func<TCoreModel, bool>> predicate);
+    }
+}
---
+++ SalesStatisticsSystem.DataAccessLayer.Contracts/ReaderWriter/ISaleDbReaderWriter.cs
@@ -0,0 +1,9 @@
+﻿using SalesStatisticsSystem.Core.Contracts.Models;
+using SalesStatisticsSystem.DataAccessLayer.Contracts.ReaderWriter.Generic;
+
+namespace SalesStatisticsSystem.DataAccessLayer.Contracts.ReaderWriter
+{
+    public interface ISaleDbReaderWriter : IGenericDbReaderWriter<SaleCoreModel>
+    {
+    }
+}
